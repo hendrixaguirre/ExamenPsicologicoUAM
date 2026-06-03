fa9d63a2fffe4848a6c04180a09f01f382712806
Jarquin23 <amjarquinc@uamv.edu.ni>
2026-06-02 18:06:48 -0600

    Creacion clase de prueba

---
+++ src/main/java/ni/edu/uam/ExamenPsicologicoUAM/modelo/AplicacionPrueba.java
@@ -0,0 +1,40 @@
+package ni.edu.uam.ExamenPsicologicoUAM.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AplicacionPrueba {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Hidden
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Sujeto sujeto;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Cuestionario cuestionario;
+
+    private LocalDate fechaAplicacion;
+
+    private LocalTime horaInicio;
+
+    private LocalTime horaFin;
+
+    private boolean finalizada;
+}
