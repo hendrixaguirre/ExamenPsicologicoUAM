a134acfd73b142fb81c7bb10e4be366329d92a70
HDAZ <hendrixag@uamv.edu.ni>
2026-06-10 21:25:22 -0600

    A Creación de clase RespuestaSujeto, y ajustes en clase Sujet

---
+++ src/main/java/ni/edu/uam/ExamenPsicologicoUAM/modelo/Sujeto.java
@@ -0,0 +1,37 @@
+package ni.edu.uam.ExamenPsicologicoUAM.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sujeto {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Hidden
+
+    private String id;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private LocalDate fechaNacimiento;
+    private int edad;
+
+    private String sexo;
+
+
+    private String estudiosRealizados;
+    private String profesion;
+}
